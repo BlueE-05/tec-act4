08352cc9fdc41f0c138d6259143c131ce3fcfe9a
BlueE-05 <danaetorres35@gmail.com>
2025-03-03 07:17:34 -0600

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,41 @@
-import Image from "next/image";
+import WeatherCard from "@/components/WeatherCard";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-blue-100 p-20">
+      <div className="relative w-full max-w-md mb-5">
+        <input type="text" placeholder="Busca una ciudad" className="w-full p-3 pl-12 text-lg border border-gray-300 rounded-full focus:outline-none focus:shadow-xl"/>
+        <svg
+          className="absolute left-5 top-3 w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-4.35-4.35M16.5 10.5a6 6 0 1 0-12 0 6 6 0 0 0 12 0z"/>
+        </svg>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className="mt-5 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+        <WeatherCard city={"Monterrey"} temperature={25.0} humidity={12.5} windSpeed={25}/>
+      </div>
+    </main>
+
   );
 }
